6657666e1fe0e2a6e947594664dafc74a44807c0
Céline <celinealamedapub@gmail.com>
2021-08-09 11:24:30 +0200

    palettes

--- src/Consts.hpp
+++ src/Consts.hpp
@@ -24,3 +24,21 @@ static const NVGcolor COLOR_GREEN_BUTTON_T = nvgRGBA(0x00, 0xba, 0x00, 0x6e);
 
 static const NVGcolor COLOR_RED_BUTTON_O = nvgRGBA(0xba, 0x00, 0x00, 0xff);
 static const NVGcolor COLOR_GREEN_BUTTON_O = nvgRGBA(0x00, 0xba, 0x00, 0xff);
+
+// palette
+// 68, 41, 80
+static const NVGcolor PALETTE_DARK_PURPLE_O = nvgRGBA(0x44, 0x29, 0x59, 0xff);
+// 118, 78, 128
+static const NVGcolor PALETTE_MEDIUM_PURPLE_O = nvgRGBA(0x76, 0x4e, 0x80, 0xff);
+// rgb(177, 96, 142)
+static const NVGcolor PALETTE_LIGHT_PURPLE_O = nvgRGBA(0xb1, 0x60, 0x8e, 0xff);
+// rgb(182, 204, 177)
+static const NVGcolor PALETTE_LIGHT_GREEN_O = nvgRGBA(0xb6, 0xcc, 0xb1, 0xff);
+// rgb(149, 175, 92)
+static const NVGcolor PALETTE_DARK_GREEN_O = nvgRGBA(0x95, 0xaf, 0x5c, 0xff);
+// transparents
+static const NVGcolor PALETTE_DARK_PURPLE_T = nvgRGBA(0x44, 0x29, 0x59, 0x6e);
+static const NVGcolor ALETTE_MEDIUM_PURPLE_T = nvgRGBA(0x76, 0x4e, 0x80, 0x6e);
+static const NVGcolor PALETTE_LIGHT_PURPLE_T = nvgRGBA(0xb1, 0x60, 0x8e, 0x6e);
+static const NVGcolor PALETTE_LIGHT_GREEN_T = nvgRGBA(0xb6, 0xcc, 0xb1, 0x6e);
+static const NVGcolor PALETTE_DARK_GREEN_T = nvgRGBA(0x95, 0xaf, 0x5c, 0x6e);
